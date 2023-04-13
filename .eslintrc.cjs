73841ab932000a1a89a3199d83a3e519fc7338f1
goodhzy <goodhzy@gmail.com>
2023-04-13 17:47:25 +0800

    feat: init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
+  },
+  plugins: ['react'],
+  rules: {
+    'import/no-absolute-path': 0,
+    'react/react-in-jsx-scope': 0
+  }
+};
