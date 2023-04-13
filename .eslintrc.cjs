88f41080643e5adbda1177e4bb4b56d7e96c107d
goodhzy <goodhzy@gmail.com>
2023-04-13 20:00:53 +0800

    chore: modify config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,17 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'plugin:prettier/recommended'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:prettier/recommended'
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
